499369b19b16581eacb33b750238d901db0648f3
TN0123 <tanaynaik149@gmail.com>
2024-04-20 12:32:45 -0400

    made updates to script.js and index.html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 function setup(){   //add a parameter to this function that represents the canvas video feed
     let countdown = 5;
     let starttime = new Date().getTime();
@@ -47,3 +49,77 @@ async function scroll(){
       //break out of the loop by checking toggle
     }
 }
+
+*/
+
+const video = document.getElementById('video')
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+    const canvas = document.getElementById('c1')
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    mainphase = false
+    const text = [
+        'Align your face in a comfortable spot in the frame'
+    ];
+    const anchor = { x: 200, y: 200 }
+    let ogx, ogy
+    const mainLoop = async () => {
+        if (!mainphase) {
+            for (let i = 0; i < 3; i++) {
+                const drawOptions = {
+                    anchorPosition: 'TOP_LEFT',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                };
+                const drawBox = new faceapi.draw.DrawTextField(text, anchor, drawOptions);
+                drawBox.draw(canvas);
+                await delay(1000);
+            }
+            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+            landmarks = detections[0].landmarks
+            ogx = landmarks._positions[54]._x
+            ogy = landmarks._positions[65]._y
+            mainphase = true;
+            return;
+        }
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        landmarks = detections[0].landmarks
+        const xpos = landmarks._positions[54]._x
+        const ypos = landmarks._positions[65]._y
+        const box = { x: xpos, y: ypos, width: 25, height: 25 }
+        let drawOptions
+        if (ypos-ogy > 10){
+            drawOptions = {
+                lineWidth: 2,
+                boxColor: "red"
+            }
+        } else if (ypos-ogy < -10){
+            drawOptions = {
+                lineWidth: 2,
+                boxColor: "green"
+            }
+        }
+        
+        const drawBox = new faceapi.draw.DrawBox(box, drawOptions)
+        drawBox.draw(document.getElementById('c1'))
+    };
+    
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    setInterval(mainLoop, 100);
+})
